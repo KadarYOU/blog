3f4454f444f591ae8a57686dd12b88cb3e71060d
Kadar YOUSSOUF WABERI <kasdartos@gmail.com>
2020-12-16 16:11:08 +0100

    form article

--- migrations/Version20201112200047.php
+++ migrations/Version20201112200047.php
@@ -6,30 +6,3 @@ namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
-
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
-final class Version20201112200047 extends AbstractMigration
-{
-    public function getDescription(): string
-    {
-        return '';
-    }
-
-    public function up(Schema $schema): void
-    {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE article DROP FOREIGN KEY FK_23A0E6667B3B43D');
-        $this->addSql('DROP INDEX IDX_23A0E6667B3B43D ON article');
-        $this->addSql('ALTER TABLE article DROP users_id');
-    }
-
-    public function down(Schema $schema): void
-    {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE article ADD users_id INT NOT NULL');
-        $this->addSql('ALTER TABLE article ADD CONSTRAINT FK_23A0E6667B3B43D FOREIGN KEY (users_id) REFERENCES users (id)');
-        $this->addSql('CREATE INDEX IDX_23A0E6667B3B43D ON article (users_id)');
-    }
-}
